056d043095430dc837dc70583e4b2349b5305511
Nikolas Jody <nikolas.jody@gmail.com>
2020-08-15 00:35:36 +0700

    styles to global styles.

--- frontend/app/containers/Profile/index.js
+++ frontend/app/containers/Profile/index.js
@@ -188,43 +188,5 @@ const _shadowStyle = {
    })
 };
 
-
-
-// const styles = StyleSheet.create({
-//    name: {
-//       fontSize: 24,
-//       marginTop: 20
-//    },
-//    description: {
-//       fontSize: 18,
-//       marginTop: 20,
-//       marginLeft: 20
-//    },
-//    descriptionText: {
-//       fontSize: 16,
-//       marginTop: 10,
-//       marginLeft: 40
-//    },
-//    tabRow: {
-//       width: '50%',
-//       alignItems: 'center', 
-//       marginTop: 10, 
-//     },
-//     tabLabelNumber: {
-//       color: 'black',
-//       fontSize: 22,
-//       textAlign: 'center',
-//       // marginBottom: 5,
-//     },
-//     tabLabelText: {
-//       color: 'black',
-//       fontSize: 14,
-//       textAlign: 'left',
-//       // marginBottom: 5,
-//     },
-//     center:{
-//       alignItems: 'center',
-//     }
-// });
 export default ProfileScreen;
 
