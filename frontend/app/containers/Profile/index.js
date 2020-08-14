7c9b5b77da9b26ffcd64d6fcb81ef99d80bdeef2
Albert Estevan <aestevan@ucsd.edu>
2020-08-14 21:38:02 +0700

    added profile switch

--- frontend/app/containers/Profile/index.js
+++ frontend/app/containers/Profile/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
-import { TouchableOpacity, View, Text , Button, FlatList} from 'react-native'
+import { StyleSheet, TouchableOpacity, View, Text , FlatList} from 'react-native'
 
-import { Icon, Container, Header, Content, Left, Right } from 'native-base';
+import { Icon, Container, Header, Content, Left, Right, Segment, Button } from 'native-base';
 
 import ProfileHeader from '../../components/ProfileHeader';
 
@@ -10,19 +10,25 @@ class  ProfileScreen extends Component {
    constructor(props) {
       super(props);
       this.state = {
+        activePage:1
       };
     }
 
-   componentDidMount() {
+    selectComponent = (activePage) => () => this.setState({activePage})
+
+    renderComponent = () => {
+        if(this.state.activePage === 1)
+          return <Component1/> //... Your Component 1 to display
+        else 
+          return <Component2/> //... Your Component 2 to display
+      }
       
-   }
-
-
    render() {
     const {navigation} = this.props;
       return (
          <Container>
              <ProfileHeader navigation={navigation} screenTitle="Profile"/>
+        
             <Content contentContainerStyle={{
                flex: 1,
                alignItems: 'center',
@@ -30,6 +36,13 @@ class  ProfileScreen extends Component {
             }}>
 
             <Text>ProfileScreen</Text>
+            <Text>{this.state.activePage}</Text>
+            <Segment>
+                    <Button active={this.state.activePage === 1}
+                        onPress={this.selectComponent(1)}><Text style={this.state.activePage === 1 ? styles.white : styles.black}>Seeking Endorsement</Text></Button>
+                    <Button  active={this.state.activePage === 2}
+                        onPress= {this.selectComponent(2)}><Text style={this.state.activePage === 2 ? styles.white : styles.black}>Endorser</Text></Button>
+            </Segment>
 
             </Content>
          </Container>
@@ -38,3 +51,19 @@ class  ProfileScreen extends Component {
    }
 }
 export default ProfileScreen
+
+const styles = StyleSheet.create({
+    white: {
+            color: '#FFFFFF',
+    },
+    black: {
+              color: '#000000',
+      },
+    red: {
+            color: '#F44336'
+      
+    },
+    orange:{
+            color : '#FF9800'
+    }
+  });
