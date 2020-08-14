0266dc12c39285641cd3f80e263cec8b5b5a14d4
Carlos Wirawan <cawirawa@ucsd.edu>
2020-08-15 03:54:41 +0700

    aman

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,186 +8,160 @@ from rest_framework.parsers import FileUploadParser
 
 from datetime import *
 
-from .models import User
-from .serializer import UserSerializer
+from .models import User, City, Bundle, Tag, Follower, Price
+from .serializer import UserSerializer, CitySerializer, BundleSerializer, TagSerializer, FollowerSerializer, PriceSerializer
+from .constants import PRICES, FOLLOWERS, CITIES, TAGS
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
     # authentication_classes = (TokenAuthentication, )
     permission_classes = (permissions.AllowAny, )
 
     @action(detail=False, methods=['POST'], permission_classes=[permissions.AllowAny])
-    def create_user(self, request, pk=None):
-        if 'name' in request.data and 'phone_number' in request.data:
-            try: 
-                user = User.objects.get(phone_number=request.data['phone_number'])
-                response = {'message': 'phone number used!'}
+    def create_profile(self, request, pk=None):
+        if 'name' in request.data and 'imageURL' in request.data and 'instaUsername' in request.data and 'phoneNumber' in request.data and 'businessNumber' in request.data and 'description' in request.data and 'tags' in request.data and 'city' in request.data and 'priceRange' in request.data and 'followers' in request.data:
+            user = User.objects.filter(phone_number=request.data['phoneNumber'])
+            if len(user) != 0:
+                response = {'message': 'User existed'}
                 return Response(response, status=status.HTTP_400_BAD_REQUEST)
-            except User.DoesNotExist:
-                newUser = User.objects.create(name=request.data['name'], phone_number=request.data['phone_number'], image_url=request.data['request_url'], instagram_username=request.data['instagram_username'], business_number=request.data['business_number'], description=request.data['description'], city_id=request.data['city_id'])
-                newUser.save()
-                serializer = UserSerializer(newUser, many=False)
-                response = {'message': 'Successfully created User', 'result': serializer.data}
-                return Response(response, status=status.HTTP_200_OK)
-            response = {'message': 'error!'}
-            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+           
+            city = City.objects.get(name=request.data['city'])
+            follower = Follower.objects.get(name=request.data['followers'])
+            price = Price.objects.get(name=request.data['priceRange'])
+            inputTags = request.data['tags']
+            newUser = User.objects.create(name=request.data['name'], phone_number=request.data['phoneNumber'], image_url=request.data['imageURL'], instagram_username=request.data['instaUsername'], business_number=request.data['businessNumber'], description=request.data['description'], city=city, follower=follower, price=price)
+            for i in inputTags:
+                tag = Tag.objects.get(name=i)
+                newUser.tags.add(tag)
+            newUser.save()
+            serializer = UserSerializer(newUser, many=False)
+            response = {'message': 'Successfully created User', 'result': serializer.data}
+            return Response(response, status=status.HTTP_200_OK)
         else:
             response = {'message': 'Please provide all attributes!'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)     
     
     @action(detail=False, methods=['POST'], permission_classes=[permissions.AllowAny])
     def update_profile(self, request, pk=None):
-        if 'name' in request.data and 'phone_number' in request.data:
+        if 'name' in request.data and 'imageURL' in request.data and 'instaUsername' in request.data and 'phoneNumber' in request.data and 'businessNumber' in request.data and 'description' in request.data and 'tags' in request.data and 'city' in request.data and 'priceRange' in request.data and 'followers' in request.data:
             try:
-                user = User.objects.get(phone_number=request.data['phone_number'])
+                user = User.objects.get(phone_number=request.data['phoneNumber'])
             except User.DoesNotExist:
                 response = {'message': 'User does not exist!'}
-                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+                return Response(response, status=status.HTTP_404_NOT_FOUND)
+            city = City.objects.get(name=request.data['city'])
+            follower = Follower.objects.get(name=request.data['followers'])
+            price = Price.objects.get(name=request.data['priceRange'])
             user.name = request.data['name']
-            user.image_url = request.data['image_url']
-            user.instagram_username = request.data['instagram_username']
-            user.phone_number = request.data['phone_number']
-            user.business_number = request.data['business_number']
+            user.image_url = request.data['imageURL']
+            user.instagram_username = request.data['instaUsername']
+            user.business_number = request.data['businessNumber']
             user.description = request.data['description']
-            user.city_id = request.data['city_id']
+            user.city = city
+            user.follower = follower
+            user.price = price
+            user.tag = []
+            inputTags = request.data['tags']
+            for i in inputTags:
+                tag = Tag.objects.get(name=i)
+                user.tags.add(tag)
             user.save()
             serializer = UserSerializer(user, many=False)
-            response = {'message': 'Successfully updated profile', 'result': serializer.data}
-            return Response(response, status=status.HTTP_200_OK)  
+            response = {'message': 'Successfully created User', 'result': serializer.data}
+            return Response(response, status=status.HTTP_200_OK)
         else:
             response = {'message': 'Please provide all attributes!'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST) 
 
     @action(detail=False, methods=['GET'])
     def get_profile(self, request, pk=None):
-        if 'phone_number' in request.headers:
+        phone = '+' + request.query_params.get('phone').strip()
+        print(phone)
+        if phone != None:
             try:
-                user = User.objects.get(phone_number=request.headers['phone_number'])
+                user = User.objects.get(phone_number=phone)
+                userPayload = UserSerializer(user, many=False).data
             except User.DoesNotExist:
                 response = {'message': 'User does not exist!'}
                 return Response(response, status=status.HTTP_404_NOT_FOUND)
-            serializer = UserSerializer(user, many=False)
-            response = {'result': serializer.data}
+            tagsPayload = []
+            for i in userPayload["tags"]:
+                tagsPayload.append(Tag.objects.get(id=i).name)
+            payload = {
+                "name": userPayload["name"],
+                "imageURL": userPayload["image_url"],
+                "instaUsername": userPayload["instagram_username"],
+                "phoneNumber": userPayload["phone_number"],
+                "businessNumber": userPayload["business_number"],
+                "description": userPayload["description"],
+                "tags": tagsPayload,
+                "city": user.city.name,
+                "priceRange": user.price.name,
+                "followers": user.follower.name,
+            }
+            response = {'result': payload}
             return Response(response, status=status.HTTP_200_OK) 
         else:
-            response = {'message': 'Please provide all attributes!'}
+            response = {'message': 'Please provide phone url parameter ex:"?phone=+1234567890'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
     
+class InitialValueViewset(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    # authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.AllowAny, )
+
+    @action(detail=False, methods=['POST'], permission_classes=[permissions.AllowAny])
+    def populate_value(self, request, pk=None):
+        if 'secretKey' in request.data:
+            if request.data['secretKey'] != "woing":
+                response = {'message': "Wrong secretKey"}
+                return Response(response, status=status.HTTP_400_BAD_REQUEST) 
+            for i in Follower.objects.all():
+                i.delete()
+            for i in Price.objects.all():
+                i.delete()
+            for i in City.objects.all():
+                i.delete()
+            for i, j in FOLLOWERS:
+                Follower.objects.create(id=i, name=j)
+            for i, j in PRICES:
+                Price.objects.create(id=i, name=j)
+            for i, j in CITIES:
+                City.objects.create(id=i, name=j)
+            for i, j in TAGS:
+                Tag.objects.update_or_create(id=i, name=j)
+            response = {'result': "Success"}
+            return Response(response, status=status.HTTP_200_OK) 
+        else:
+            response = {'message': 'Please provide the secretKey'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['GET'])
+    def tags(self, request, pk=None):
+        tags = Tag.objects.all()
+        tagsPayload = TagSerializer(tags, many=True).data
+        payload = []
+        for i in tagsPayload:
+            payload.append(i["name"])
+        response = {'result': payload}
+        return Response(response, status=status.HTTP_200_OK) 
     
-
+    @action(detail=False, methods=['GET'])
+    def cities(self, request, pk=None):
+        cities = City.objects.all()
+        citiesPayload = CitySerializer(cities, many=True).data
+        payload = []
+        for i in citiesPayload:
+            payload.append(i["name"])
+        response = {'result': payload}
+        return Response(response, status=status.HTTP_200_OK)
     
-# Create your views here.
-# class ProfileViewSet(viewsets.ModelViewSet):
-#     queryset = Profile.objects.all()
-#     serializer_class = ProfileSerializer
-#     authentication_classes = (TokenAuthentication, )
-#     permission_classes = (permissions.AllowAny, )
-#     parser_class = (FileUploadParser,)
-
-#     @action(detail=False, methods=['GET'])
-#     def get_profile(self, request, pk=None):
-#         if 'email' in request.headers:
-#             try:
-#                 user = User.objects.get(email=request.headers['email'])
-#                 obj = Profile.objects.get(user=user)
-#             except User.DoesNotExist:
-#                 response = {'message': 'User does not exist!'}
-#                 return Response(response, status=status.HTTP_404_NOT_FOUND)
-#             except Profile.DoesNotExist:
-#                 response = {'message': 'Profile does not exist!'}
-#                 return Response(response, status=status.HTTP_404_NOT_FOUND)
-#             serializer = ProfileSerializer(obj, many=False)
-#             response = {'result': serializer.data}
-#             return Response(response, status=status.HTTP_200_OK) 
-#         else:
-#             response = {'message': 'Please provide all attributes!'}
-#             return Response(response, status=status.HTTP_400_BAD_REQUEST)
-    
-#     @action(detail=False, methods=['POST'], permission_classes=[permissions.AllowAny])
-#     def create_user(self, request, pk=None):
-#         if 'email' in request.data and 'name' in request.data and 'username' in request.data:
-#             try: 
-#                 user = User.objects.get(username=request.data['username'])
-#                 response = {'message': 'username used!'}
-#                 return Response(response, status=status.HTTP_400_BAD_REQUEST)
-#             except User.DoesNotExist:
-#                 try: 
-#                     user = User.objects.get(email=request.data['email'])
-#                     response = {'message': 'email used!'}
-#                     return Response(response, status=status.HTTP_400_BAD_REQUEST)
-#                 except User.DoesNotExist:
-#                     newUser = User.objects.create(username=request.data['username'], email=request.data['email'])
-#                     print("woiks")
-#                     newUser.first_name = request.data['name']
-#                     newUser.save()
-#                     obj = Profile.objects.create(user=newUser)
-#                     obj.save()
-#                     serializer = ProfileSerializer(obj, many=False)
-#                     response = {'message': 'Successfully created User', 'result': serializer.data}
-#                     return Response(response, status=status.HTTP_200_OK)
-#             response = {'message': 'error!'}
-#             return Response(response, status=status.HTTP_400_BAD_REQUEST)
-#         else:
-#             response = {'message': 'Please provide all attributes!'}
-#             return Response(response, status=status.HTTP_400_BAD_REQUEST)
-
-#     @action(detail=False, methods=['GET'])
-#     def log_in(self, request, pk=None):
-#         if 'email' in request.headers:
-#             try: 
-#                 user = User.objects.get(email=request.headers['email'])
-#                 obj = Profile.objects.get(user=user)
-#                 serializer = ProfileSerializer(obj, many=False)
-#                 response = {'message': 'Successfully fetched User', 'result': serializer.data}
-#                 return Response(response, status=status.HTTP_200_OK)
-#             except User.DoesNotExist:
-#                 response = {'message': 'User does not exist!'}
-#                 return Response(response, status=status.HTTP_400_BAD_REQUEST)
-#         else:
-#             response = {'message': 'Please provide all attributes!'}
-#             return Response(response, status=status.HTTP_400_BAD_REQUEST)
-            
-#     @action(detail=False, methods=['POST'], permission_classes=[permissions.AllowAny])
-#     def update_profile(self, request, pk=None):
-#         if 'email' in request.data and 'name' in request.data and 'phone' in request.data and 'age' in request.data and 'lat' in request.data and 'lon' in request.data and 'sports' in request.data and 'bio' in request.data  and 'skill' in request.data:
-#             try:
-#                 user = User.objects.get(email=request.data['email'])
-#             except User.DoesNotExist:
-#                 response = {'message': 'User does not exist!'}
-#                 return Response(response, status=status.HTTP_400_BAD_REQUEST)
-#             profile = Profile.objects.get(user=user)
-#             user.first_name = request.data['name']
-#             profile.phone = request.data['phone']
-#             profile.age = request.data['age']
-#             profile.lon = request.data['lon']
-#             profile.lat = request.data['lat']
-#             profile.sports = request.data['sports']
-#             profile.bio = request.data['bio']
-#             profile.skill = request.data['skill']
-#             user.save()
-#             profile.save()
-#             serializer = ProfileSerializer(profile, many=False)
-#             response = {'message': 'Successfully updated profile', 'result': serializer.data}
-#             return Response(response, status=status.HTTP_200_OK)  
-#         else:
-#             response = {'message': 'Please provide all attributes!'}
-#             return Response(response, status=status.HTTP_400_BAD_REQUEST)    
-
-#     @action(detail=False, methods=['POST'], permission_classes=[permissions.AllowAny])
-#     def update_image(self, request, pk=None):
-#         data = request.data
-#         if "email" in data:
-#             try:
-#                 user = User.objects.get(email=data.get("email"))
-#             except User.DoesNotExist:
-#                 response = {'message': 'User does not exist!'}
-#                 return Response(response, status=status.HTTP_400_BAD_REQUEST)
-#             profile = Profile.objects.get(user=user)
-#             profile.profile_image = data.get("profile_image")
-#             profile.save()
-#             serializer = ProfileSerializer(profile, many=False)
-#             response = {'message': 'Successfully updated profile image', 'result': serializer.data}
-#             return Response(response, status=status.HTTP_200_OK)  
-#         else:
-#             response = {'message': 'Please provide all attributes!'}
-#             return Response(response, status=status.HTTP_400_BAD_REQUEST)    
+    @action(detail=False, methods=['GET'])
+    def prices(self, request, pk=None):
+        prices = Price.objects.all()
+        pricesPayload = PriceSerializer(prices, many=True).data
+        payload = []
+        for i in pricesPayload:
+            payload.append(i["name"])
+        response = {'result': payload}
+        return Response(response, status=status.HTTP_200_OK) 
