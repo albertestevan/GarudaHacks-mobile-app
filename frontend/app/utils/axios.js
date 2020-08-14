22295a1143aaec204cf3fb033e80e94df8caf9c5
Albert Estevan <aestevan@ucsd.edu>
2020-08-14 17:49:30 +0700

    asdf

--- frontend/app/utils/axios.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-unresolved
-import { create } from 'axios';
-
-const axiosInstance = create({
-  baseURL: '',
-  /* other custom settings */
-});
-export default axiosInstance;
