980d109132235f59c8903903083c3443e737d1c8
Albert Estevan <aestevan@ucsd.edu>
2020-08-15 04:04:52 +0700

    progress filter location

--- frontend/app/containers/FilterLocation/index.js
+++ frontend/app/containers/FilterLocation/index.js
@@ -1,24 +1,77 @@
-import React from 'react'
-import { TouchableOpacity, View, Text, StyleSheet, Button} from 'react-native'
+import React, { Component } from 'react';
+import { TouchableOpacity, View, Text, StyleSheet, Button} from 'react-native';
 
-import { Icon, Container, Header, Content, Left} from 'native-base'
+import { Icon, Container, Header, Content, Left, Right, List, ListItem} from 'native-base';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
-const FilterLocationScreen = ({ navigation }) => {
-  
-   return (
-      <Container>
 
-         <Content contentContainerStyle={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center'
-         }}>
+class FilterLocationScreen extends React.Component {
 
-         <Text>FilterLocationScreen</Text>
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+      }
+    
 
-         </Content>
-      </Container>
-   )
+    render() {
+        const menuList = 
+        [
+            //Haven't called api for city list
+            "Ambon",
+            "Balikpapan",
+            "Banda Aceh",
+            "Bandung",
+            "Banjar",
+            "Banjarbaru",
+        ]
+        return (
+            <Container>
+
+                <Content contentContainerStyle={{
+                    
+                }}>
+            
+            <List>
+                {menuList.map(menu => (
+                <ListItem onPress={() => this.setState({ [menu]: !this.state[menu]})} selected>
+                  <Left>
+                    <Content contentContainerStyle={{
+                          padding: 10,
+                          flexDirection: 'row',
+                        }}>
+                        {this.state[menu] ? (
+                        <Ionicons name="md-checkmark" size={20} style={styles.pickedText}/>
+                        ) : null}
+
+                        {this.state[menu] ? (
+                        <Text style={styles.pickedText}>{menu}</Text>
+                        ) : <Text style={styles.regularText}>{menu}</Text>}
+                    </Content>
+                  </Left>
+                  <Right>
+                    <Icon name="arrow-forward" />
+                  </Right>
+              </ListItem>))}
+              </List>
+
+                </Content>
+            </Container>
+        )
+    };
 }
-export default FilterLocationScreen
+
+export default FilterLocationScreen;
+
+const styles = StyleSheet.create({
+    pickedText:{
+        color: '#32CD32',
+        fontWeight: 'bold',
+        paddingRight: 20
+    },
+    regularText:{
+        fontWeight: 'bold',
+    }
+});
