550d1207d84ef51778f66b99bf74696b38a25b16
Nikolas Jody <nikolas.jody@gmail.com>
2020-08-15 23:22:33 +0700

    aiueo, api kirim image tested

--- frontend/app/containers/CameraWrapper/index.js
+++ frontend/app/containers/CameraWrapper/index.js
@@ -1,10 +1,39 @@
 import React, {useState} from 'react'
-import { TouchableOpacity, View, Text, StyleSheet, Button, Image} from 'react-native'
-
-import { Icon, Container, Header, Content, Left, Spinner} from 'native-base'
+import { TouchableOpacity, View, Text, StyleSheet, Image, Alert} from 'react-native'
+import { Icon, Container, Header, Content, Left, Spinner, Button} from 'native-base'
 import Media from '../../components/Media'
 function CameraWrapper(){
   const [image, setImage] = useState(null);
+
+  const submitHandler= async()=>{
+      console.log(image);
+      const form = new FormData();
+      form.append("file", {uri: image, name: 'profileImage.jpg', type: 'image/jpeg'})
+
+      const response = await fetch('http://165.227.25.15/api/file/', {
+      method: 'POST',
+      body: form
+      }).catch(function(error) {
+         console.log('There has been a problem with your fetch operation: ');
+         // ADD THIS THROW error
+         // throw error;
+      });
+      const resData = await response.json();
+      console.log(resData);
+      if (resData.error) {
+            let message = resData.error;
+            console.log(resData.error);
+            // throw new Error(message);
+            Alert.alert(message);
+            return
+      }else{
+         console.log("success resp", resData);
+      }
+
+
+  }
+//   console.log('imh', image)
+
    return (
       <Container>
          <Content contentContainerStyle={{
@@ -18,8 +47,22 @@ function CameraWrapper(){
              <Media description="camera" mediaType="camera" setImage={setImage}/>
              <Media description="both" setImage={setImage}/>
             {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+            <Button disabled={image ? false : true} onPress={()=> submitHandler()}><Text>Submit</Text></Button>
          </Content>
       </Container>
    )
 }
-export default CameraWrapper
+export default CameraWrapper;
+
+// const form = new FormData();
+
+// form.append('image', {
+//   uri: "file:///...",
+//   type: 'image/jpg',
+//   name: 'image.jpg',
+// });
+
+// fetch('https://example.com/api/upload', {
+//   method: 'POST',
+//   body: form
+// });
