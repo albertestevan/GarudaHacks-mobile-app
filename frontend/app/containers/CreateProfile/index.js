440713d95c7abeb7025a183a36cedf8a3a3e6ac7
Albert Estevan <aestevan@ucsd.edu>
2020-08-16 00:07:38 +0700

    initialValues progress 1

--- frontend/app/containers/CreateProfile/index.js
+++ frontend/app/containers/CreateProfile/index.js
@@ -2,7 +2,6 @@ import React , { Component } from 'react';
 import allReducers from '../../reducers/index.js';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import LoginForm from '../../components/LoginForm';
 import { Field, reduxForm } from 'redux-form';
 import CreateProfileForm from '../../components/CreateProfileForm/index.js';
 const store = createStore(allReducers);
