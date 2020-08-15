b34279ccd8f02aded355cbb36b87a6bf1eb11ddd
Albert Estevan <aestevan@ucsd.edu>
2020-08-15 18:20:40 +0700

    create profile progress 1

--- frontend/app/containers/SignUpScreen/index.js
+++ frontend/app/containers/SignUpScreen/index.js
@@ -1,32 +1,16 @@
-import React from 'react';
-import { reduxForm, Field } from 'redux-form';
-import { ScrollView, Text, TouchableOpacity } from 'react-native';
-
-import MyTextInput from '../../components/ReduxFormInput';
-
-function MyForm(props) {
-
-    const formStates = ['asyncValidating', 'dirty', 'pristine', 'valid', 'invalid', 'submitting',
-    'submitSucceeded', 'submitFailed'];
-
-  return (
-    <ScrollView keyboardShouldPersistTaps={'handled'}>
-      <Text>Email</Text>
-      <Field
-        name={'email'}
-        component={MyTextInput}
-      />
-      <Text>The form is:</Text>
-      {
-        formStates.filter((state) => props[state]).map((state) => {
-          return <Text key={state}> - { state }</Text>
-        })
-      }
-      <TouchableOpacity onPress={props.handleSubmit}>
-        <Text>Submit!</Text>
-      </TouchableOpacity>
-    </ScrollView>
-  );
+import React , { Component } from 'react';
+import allReducers from '../../reducers/index.js';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import SimpleForm from '../../components/SimpleForm';
+import { Field, reduxForm } from 'redux-form';
+const store = createStore(allReducers);
+ export default class Application extends Component{
+  render(){
+    return(
+      <Provider store= {store}>
+        <SimpleForm />
+      </Provider>
+    )
+  }
 }
-
-export default reduxForm({ form: 'signUp' })(MyForm);
